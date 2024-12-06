5ec6949c0559e1d1e1b74cd771194b155013aeba
girraj <girraj.rathore94@gmail.com>
2024-12-06 07:35:09 +0530

    feat : added the User Class

---
+++ src/oops4/User.java
@@ -0,0 +1,42 @@
+package oops4;
+
+public abstract class User {
+
+    private int userId;
+    private String name;
+    private String contactInfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public User(){
+        this.userId = 0;
+    }
+
+    public User(int userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public User(User user){
+        this.userId = user.userId;
+        this.name = user.name;
+        this.contactInfo = user.contactInfo;
+    }
+
+    abstract void  displayDashboard();
+    abstract boolean canBorrowBooks();
+}
