f31592a800eab169fd2413e9d072a65b5d11f2c1
girraj <girraj.rathore94@gmail.com>
2024-12-20 08:45:00 +0530

    feat : Milestone 2: Bound with Item only

--- src/CollectionLab/InventoryClient.java
+++ src/CollectionLab/InventoryClient.java
@@ -3,21 +3,24 @@ package CollectionLab;
 public class InventoryClient {
 
     public static void main(String[] args) {
-        Inventory inventory = new Inventory();
+        Inventory<Item> inventory = new Inventory();
         inventory.addItem(new Book("1", "Book1", 100, 10, "Author1"));
         inventory.addItem(new Book("2", "Book2", 200, 20, "Author2"));
         inventory.addItem(new Electronics("3", "Electronics1", 300, 30, 1));
         inventory.addItem(new Electronics("2", "Electronics2", 400, 40, 2));
 
         inventory.printInventory();
-
+        System.out.println("-------------");
         System.out.println(inventory.getItem("1"));
         System.out.println(inventory.getItem("2"));
 
+        System.out.println("-------------");
         inventory.removeItem("1");
         inventory.printInventory();
 
+        System.out.println("-------------");
         inventory.removeItem("2");
+        System.out.println("-------------");
         inventory.printInventory();
     }
 }
